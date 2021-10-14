1b42f5e7bb1130caa80ed5bb876c75860bc80564
aschemily <aschemily@gmail.com>
2021-10-14 12:01:13 -0400

    Initial Commit

---
+++ ezFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ezFit
+//
+//  Created by Emily Asch on 10/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
